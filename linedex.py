a3e017b37ce99ef8111aaed14a46e3020da1ac4a
james <jamesdaniel2197@gmail.com>
2018-07-08 20:12:54 +0100

    remove linedex.py

--- linedex.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-with open("linedefs.bin", "rb") as f:
-  linedefs = np.frombuffer(f.read(), dtype=np.uint8)
-  linedefs = linedefs.reshape((-1, 2))
-  linedefs = np.flip(linedefs, 1)
-  with open("linedefs.new.bin", "wb") as f:
-    f.write(linedefs.tobytes())
